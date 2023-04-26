5fb8885e06fdbdc1acbf44afbcd85d32a096d15a
hreenir <hreenir12@gmailc.com>
2023-04-26 11:39:08 +0300

    Deleted unused HotelMapRepository and HotelRepository now use only HotelJpaRepository

--- booking-service/src/main/java/ru/otus/hotelsbooker/repository/HotelJpaRepository.java
+++ booking-service/src/main/java/ru/otus/hotelsbooker/repository/HotelJpaRepository.java
@@ -5,7 +5,7 @@ import ru.otus.hotelsbooker.model.Hotel;
 
 import java.util.List;
 
-public interface HotelJpaRepository extends JpaRepository<Hotel, Long>, HotelRepository {
+public interface HotelJpaRepository extends JpaRepository<Hotel, Long> {
     List<Hotel> findAllByCityIgnoreCase(String name);
     Hotel findAllById(long id);
 
