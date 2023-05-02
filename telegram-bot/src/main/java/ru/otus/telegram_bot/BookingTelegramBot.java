54686a8334d283a551124491358d6002894ff1f4
A.Polyakov <a.polyakov@unlimint.com>
2023-05-02 16:12:49 +0300

    fix hotel not found in addRoom

--- telegram-bot/src/main/java/ru/otus/telegram_bot/BookingTelegramBot.java
+++ telegram-bot/src/main/java/ru/otus/telegram_bot/BookingTelegramBot.java
@@ -1,21 +1,19 @@
 package ru.otus.telegram_bot;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.MessageEntity;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import ru.otus.telegram_bot.config.BotConfigurationProperties;
 
-import java.util.List;
+import java.util.Optional;
 
 @Component
 @Slf4j
@@ -37,17 +35,30 @@ public class BookingTelegramBot  extends TelegramLongPollingBot {
         String messageText = update.getMessage().getText();
         Long chatId = update.getMessage().getChatId();
         String username = update.getMessage().getFrom().getUserName();
-        String city = "Moscow";
+        Optional<MessageEntity> commandEntity = update.getMessage().getEntities()
+                .stream().filter(e -> "bot_command".equals(e.getType())).findFirst();
+        if (commandEntity.isPresent()) {
+            String command =
+                    update.getMessage()
+                            .getText()
+                            .substring(commandEntity.get().getOffset(), commandEntity.get().getLength());
+            switch (command) {
+                case "start":
+                    callback(chatId, username, "COMMAND LIST");
+                    break;
+            }
+        }
+            String city = "Moscow";
         // /search city=Moscow
         // switch-case: "search": hotelBokkerCLient.search(city=Moscow)
-        ResponseEntity<List> forEntity = restTemplate.getForEntity("http://localhost:8081/hotel?city=" + city, List.class);
+        //ResponseEntity<List> forEntity = restTemplate.getForEntity("http://localhost:8081/hotel?city=" + city, List.class);
         // GET localhost:8881/hotel?city=Moscow
         // List<Hotel> hotels
-        String valueAsString = objectMapper.writeValueAsString(forEntity.getBody());
-        pingPongBot(chatId, username, valueAsString);
+        //String valueAsString = objectMapper.writeValueAsString("COMMAND LIST");
+        //pingPongBot(chatId, username, "COMMAND LIST");
     }
 
-    private void pingPongBot(long chatId, String userName, String messageText) {
+    private void callback(long chatId, String userName, String messageText) {
         SendMessage message = new SendMessage();
         message.setChatId(chatId);
         message.setText(messageText);
