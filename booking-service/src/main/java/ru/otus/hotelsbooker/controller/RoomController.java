d8aa674f5dee4a1fefd65b03ada45f63968af565
tryCodingMaks <learnjavamaks@gmail.com>
2023-05-11 02:14:34 +0300

    Неизвестно насколько я все вспомнил и вернул...

--- booking-service/src/main/java/ru/otus/hotelsbooker/controller/RoomController.java
+++ booking-service/src/main/java/ru/otus/hotelsbooker/controller/RoomController.java
@@ -1,4 +1,62 @@
 package ru.otus.hotelsbooker.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import ru.otus.dto.RoomDto;
+import ru.otus.hotelsbooker.service.RoomService;
+
+@RestController
+@RequestMapping("/room")
 public class RoomController {
+    @Autowired
+    private RoomService roomService;
+
+    /**
+     * GET localhost:8881/room?hotelId=1
+     *
+     * @param hotelId
+     * @return
+     */
+    @GetMapping
+    public List<RoomDto> getAllRooms(@RequestParam(name = "hotelId") Long hotelId) {
+        return roomService.getHotelById(hotelId);
+    }
+
+    /**
+     * GET localhost:8881/room/{id}
+     *
+     * @param id
+     * @return
+     */
+    @GetMapping("/{id}")
+    public RoomDto getRoomById(@PathVariable Long id) {
+        return roomService.getRoomById(id);
+    }
+
+    /**
+     * POST localhost:8881/room
+     * body {}
+     *
+     * @param room
+     * @return
+     */
+    @PostMapping(consumes = "application/json", produces = "application/json")
+    public RoomDto createNewRoom(@RequestBody RoomDto room) {
+        return roomService.createNewRoom(room);
+    }
+
+    /**
+     * PUT localhost:8881/room/{id}
+     * body {}
+     *
+     * @param id
+     * @param room
+     * @return
+     */
+    @PutMapping(path = "/{id}", consumes = "application/json", produces = "application/json")
+    public RoomDto updateRoom(@PathVariable Long id, @RequestBody RoomDto room) {
+        return roomService.updateRoom(id, room);
+    }
 }
--- booking-service/src/test/java/ru/otus/hotelsbooker/model/RoomServiceTest.java
+++ booking-service/src/test/java/ru/otus/hotelsbooker/model/RoomServiceTest.java
@@ -1,7 +1,6 @@
 package ru.otus.hotelsbooker.model;
 
 import jakarta.transaction.Transactional;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +12,8 @@ import ru.otus.hotelsbooker.service.RoomService;
 
 import java.math.BigDecimal;
 import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 @Transactional
@@ -27,23 +28,47 @@ public class RoomServiceTest {
     @DisplayName("Тестирование успешного добавления апартаментов в отель")
     void testSuccessfullyAddRoomToAHotel() {
         HotelDto hotelDto = hotelService.createNewHotel(new HotelDto("Hilton", "Moscow", "Russia", "address"));
-        RoomDto roomDtoFirst = roomService.addRoom(new RoomDto(1L,"Single", 1, new BigDecimal(100)), hotelDto.getId());
-        RoomDto roomDtoSecond = roomService.addRoom(new RoomDto(2L,"Double", 2, new BigDecimal(100)), hotelDto.getId());
+        RoomDto roomDtoFirst = roomService.addRoom(new RoomDto(1L, "Single", 1, new BigDecimal(100)), hotelDto.getId());
+        RoomDto roomDtoSecond = roomService.addRoom(new RoomDto(2L, "Double", 2, new BigDecimal(100)), hotelDto.getId());
         List<RoomDto> expected = List.of(roomDtoFirst, roomDtoSecond);
         List<RoomDto> actual = hotelService.getHotelById(hotelDto.getId()).getRooms();
-        Assertions.assertEquals(expected, actual);
+        assertEquals(expected, actual);
     }
+
     @Test
     @DisplayName("Тестирование связности апартоментов с отелем")
     void testThatHotelIdMatchesWithHotelIdAddedRoom() {
         HotelDto hotelDto = hotelService.createNewHotel(new HotelDto("Hilton", "Moscow", "Russia", "address"));
         Hotel createdHotel = hotelService.getHotelRepository().findAllById(hotelDto.getId());
         roomService.addRoom(new RoomDto("Single", 1, new BigDecimal(100)), hotelDto.getId());
-        List <Room> rooms = createdHotel.getRooms();
+        List<Room> rooms = createdHotel.getRooms();
         Room room = rooms.get(0);
 
         long expected = room.getHotel().getId();
         long actual = createdHotel.getId();
-        Assertions.assertEquals(expected, actual);
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    @DisplayName ("Тестирование обновления комнат")
+    void updateRoom() {
+        Room room = new Room();
+        room.setId(1L);
+        room.setName("single");
+        room.setCapacity(1);
+        room.setPriceByDay(BigDecimal.valueOf(100));
+
+        RoomDto roomDto = new RoomDto();
+        roomDto.setName("double");
+        roomDto.setCapacity(2);
+        roomDto.setPriceByDay(BigDecimal.valueOf(200));
+
+        RoomDto updatedRoomDto = roomService.updateRoom(1L, roomDto);
+
+        assertEquals(roomDto.getName(), updatedRoomDto.getName());
+        assertEquals(roomDto.getId(), updatedRoomDto.getId());
+        assertEquals(roomDto.getPriceByDay(), updatedRoomDto.getPriceByDay());
+        assertEquals(roomDto.getCapacity(), updatedRoomDto.getCapacity());
     }
 }
